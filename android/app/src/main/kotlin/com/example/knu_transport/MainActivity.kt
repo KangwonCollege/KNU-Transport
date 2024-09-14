d3e2cfcf85579cf7348feb7ee1545e81bf7e013a
gunyu1019 <gunyu1019@gmail.com>
2024-09-14 05:42:03 +0000

    [Init] First Commit

---
+++ android/app/src/main/kotlin/com/example/knu_transport/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.knu_transport
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/inner_bus_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InnerBusPage extends StatelessWidget {
+  const InnerBusPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Text("안녕?"),
+    );
+  }
+}
