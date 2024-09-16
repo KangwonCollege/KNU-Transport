0a0412b4cae23af5bbba13a7f3bf2aa03e2d0731
gunyu1019 <gunyu1019@gmail.com>
2024-09-16 11:27:29 +0000

    [Feat] PageView

--- lib/components/inner_bus_arrival.dart
+++ lib/components/inner_bus_arrival.dart
@@ -1,23 +1,24 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:knu_transport/models/station_info.dart';
-import 'package:knu_transport/utilities/load_asset.dart';
 
 class InnerBusArrival extends StatelessWidget {
-  InnerBusArrival({super.key, required List<StationInfo> stations});
+  InnerBusArrival({super.key, required List<StationInfo> stations, required PageController controller})
+      : this.station = stations,
+        this.controller = controller;
 
-  final List<StationInfo> stations = List.empty();
+  List<StationInfo> station = List.empty();
+  late PageController controller;
 
-  Widget card(StationInfo stationInfo) {
-    final DateTime nowTime = DateTime.now();
-
-    return const Card(
+  Widget card(StationInfo info) {
+    // final DateTime nowTime = DateTime.now();
+    return Card(
       child: Column(
         children: <Widget>[
           // [UX] 특정 버튼을 누르면 반댓편 정류장을 알 수 있도록
           // [TEXT] MM분 후 출발 / 곧 도착 / 운행 종료
+          Text(info.name)
         ],
       ),
     );
@@ -25,13 +26,14 @@ class InnerBusArrival extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    PageController pageController = PageController(initialPage: 0);
-
-    return PageView.builder(
-        itemCount: stations.length, // 정류장의 갯수
-        controller: pageController, // 사용자의 위치를 기준으로 가장 가까운 정류소를 위치 시킬 예정
+    return SizedBox(
+      height: 200,
+      child: PageView.builder(
+        itemCount: station.length, // 정류장의 갯수
+        controller: controller, // 사용자의 위치를 기준으로 가장 가까운 정류소를 위치 시킬 예정
         itemBuilder: (BuildContext context, int index) {
-          return card(stations[index]);
-        });
+          return card(station[index]);
+        })
+    );
   }
 }
--- lib/screens/inner_bus_page.dart
+++ lib/screens/inner_bus_page.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:knu_transport/components/inner_bus_map.dart';
 import 'package:knu_transport/components/inner_bus_arrival.dart';
-import 'package:knu_transport/models/initalization.dart';
 import 'package:knu_transport/models/station_info.dart';
 import 'package:knu_transport/utilities/load_asset.dart';
 
@@ -10,42 +9,51 @@ class InnerBusPage extends StatefulWidget {
   const InnerBusPage({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    return _InnerBusPageState();
-  }
+  _InnerBusPageState createState() => _InnerBusPageState();
 }
 
 class _InnerBusPageState extends State<InnerBusPage> {
-  Future<List<StationInfo>> loadStationInfo() async {
-    List<StationInfo> station = await loadAssets(
-        "assets/data/inner_bus/station_info.json", StationInfo.fromJson);
-    return station;
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 0);
   }
+
+  late PageController _pageController;
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: <Widget>[
-        Text(
+        const Text(
           "교내 순환버스",
           style: TextStyle(
               color: Color(0xff000000), decoration: TextDecoration.none),
           textAlign: TextAlign.start,
         ),
-        FutureBuilder(
-          future: Future.wait([this.loadStationInfo]),
-          builder: (BuildContext context, AsyncSnapshot<List<List<StationInfo>> snapshot) {
-            if (!snapshot.hasData) return CircularProgressIndicator();
+        FutureBuilder<List<List<StationInfo>>>(
+          future: Future.wait([loadStationInfo()]),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasError) print(snapshot.error);
+            if (!snapshot.hasData) return const CircularProgressIndicator();
             return Column(
               children: [
-                InnerBusMap(), // 지도
+                const InnerBusMap(), // 지도
                 InnerBusArrival(
-                    stations: (snapshot.data as Initalization).stations)
+                  stations: (snapshot.data[0]),
+                  controller: _pageController,
+                )
               ],
             );
           },
         )
       ],
     );
+  }
+
+  Future<List<StationInfo>> loadStationInfo() async {
+    List<StationInfo> station = await loadAssets(
+        "assets/data/inner_bus/station_info.json", StationInfo.fromJson);
+    return station;
   }
 }
