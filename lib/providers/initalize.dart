c82efd114c2ebe4eadadf72cf2a6b4935601dc4a
gunyu1019 <gunyu1019@gmail.com>
2024-09-20 14:56:37 +0000

    [Feat] Make a dataTimetableProvider futureProvider method (Dirty Code)

--- lib/providers/initalize.dart
+++ lib/providers/initalize.dart
@@ -16,6 +16,14 @@ final dataRouteInfoProvider = FutureProvider<List<List<double>>>((ref) async {
     return routeInfo;
 });
 
-final dataTimetableProvider = FutureProvider<List<List<TimeOfDay>>>((ref) async {
-
+final dataTimetableProvider = FutureProvider<List<List<Timetable>>>((ref) async {
+  var timetableInfo = await loadJson<List<dynamic>>("assets/data/inner_bus/timetable.json");
+  // print(timetableInfo);
+  List<List<Timetable>> result = List.filled(timetableInfo[0].length, List.empty(growable: true));
+  for(MapEntry<int, List<dynamic>> data1 in timetableInfo.cast<List<dynamic>>().asMap().entries) {
+    for(MapEntry<int, Map<dynamic, dynamic>> data2 in data1.value.cast<Map<dynamic, dynamic>>().asMap().entries) {
+      result[data2.key].add(Timetable.fromJson(data2.value.cast<String, int>(), data1.key));
+    }
+  }
+  return result;
 });
