6c5dc04bed14df01b1e1213e614d9ec90d3fb96f
gunyu1019 <gunyu1019@gmail.com>
2024-09-21 05:12:19 +0000

    [Feat] Add getTextSize Method

---
+++ lib/utilities/text_size.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+
+Size getTextSize(String text, TextStyle style) {
+  final TextPainter textPainter = TextPainter(
+      text: TextSpan(text: text, style: style),
+      maxLines: 1,
+      textDirection: TextDirection.ltr)
+    ..layout(minWidth: 0, maxWidth: double.infinity);
+  return textPainter.size;
+}
