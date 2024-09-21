15b9d76986079c9a7dbd1d9e4cf6de76423773f8
gunyu1019 <gunyu1019@gmail.com>
2024-09-21 14:46:21 +0000

    [Format] Re-Formatting with "dart format" cli

--- lib/widgets/text_skeleton.dart
+++ lib/widgets/text_skeleton.dart
@@ -1,9 +1,6 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:knu_transport/utilities/text_size.dart';
 import 'package:skeleton_text/skeleton_text.dart';
-
 
 class SkeletonText extends StatelessWidget {
   final double width;
@@ -11,37 +8,35 @@ class SkeletonText extends StatelessWidget {
   final Color color;
   final Color shimmerColor;
 
-  const SkeletonText({
-    super.key,
-    required this.width, 
-    required this.height, 
-    this.color = Colors.grey,
-    this.shimmerColor = Colors.white54
-  });
+  const SkeletonText(
+      {super.key,
+      required this.width,
+      required this.height,
+      this.color = Colors.grey,
+      this.shimmerColor = Colors.white54});
 
-  SkeletonText.fromText({
-    super.key,
-    required String text, 
-    required TextStyle style,
-    Color? color,
-    Color? shimmerColor
-  }) : width = getTextSize(text, style).width,
-    height = getTextSize(text, style).height,
-    color = color ?? Colors.grey,
-    shimmerColor = color ?? Colors.white54;
+  SkeletonText.fromText(
+      {super.key,
+      required String text,
+      required TextStyle style,
+      Color? color,
+      Color? shimmerColor})
+      : width = getTextSize(text, style).width,
+        height = getTextSize(text, style).height,
+        color = color ?? Colors.grey,
+        shimmerColor = color ?? Colors.white54;
 
   @override
   Widget build(BuildContext context) {
     return SkeletonAnimation(
-    borderRadius: BorderRadius.circular(height / 3),
-    shimmerColor: Colors.white54,
-    child: Container(
-      height: height,
-      width: width,
-      decoration: BoxDecoration(
       borderRadius: BorderRadius.circular(height / 3),
-      color: color),
+      shimmerColor: Colors.white54,
+      child: Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(height / 3), color: color),
       ),
-  );
+    );
   }
 }
