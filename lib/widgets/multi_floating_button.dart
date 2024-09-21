cb86eca04986feb44caa2e365b8cc8a0f5a9cc1c
gunyu1019 <gunyu1019@gmail.com>
2024-09-21 14:47:27 +0000

    [Feat] Add multi floating button widget

---
+++ lib/widgets/multi_floating_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MultiFloatingButton extends StatelessWidget {
+  final List<IconData> icon;
+  final int size;
+  final void Function(int) onClickListener;
+
+  const MultiFloatingButton({
+    super.key,
+    required this.icon,
+    required this.onClickListener,
+    this.size = 30,
+  });
+
+  Widget floatingButton(int index) {
+    return FloatingActionButton(
+      onPressed: () {
+        onClickListener(index);
+      },
+      shape: const CircleBorder(),
+      child: Icon(
+        icon[index],
+        size: size.toDouble(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+        children: icon.asMap().entries.map((element) {
+      return Align(
+          alignment: Alignment(Alignment.bottomRight.x,
+              Alignment.bottomRight.y - (0.2 * element.key)),
+          child: floatingButton(element.key));
+    }).toList());
+  }
+}
