e8955df5a7c0280ce3074b16186aa32ca1984c81
gunyu1019 <gunyu1019@gmail.com>
2024-09-21 11:27:27 +0000

    [Feat] (WIP) Add Floating button for location, refresh ... etc

--- lib/screens/inner_bus_page.dart
+++ lib/screens/inner_bus_page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:carousel_slider/carousel_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -21,10 +22,10 @@ class _InnerBusPageState extends ConsumerState<InnerBusPage> {
   @override
   void initState() {
     super.initState();
-    _pageController = PageController(initialPage: 0);
+    _pageController = CarouselSliderController();
   }
 
-  late PageController _pageController;
+  late CarouselSliderController _pageController;
   late TabController _tabController;
   late NaverMapController _mapController;
 
@@ -34,29 +35,53 @@ class _InnerBusPageState extends ConsumerState<InnerBusPage> {
     final pageSize = Size(mediaQuery.size.width, mediaQuery.size.height);
 
     final routeInfo = ref.watch(dataRouteInfoProvider);
-    routeInfo.whenData((routeInfo) {
-      print(routeInfo);
-    });
-    if (routeInfo.hasError) {
-      print(routeInfo.error.toString());
-    }
 
     return Scaffold(
         backgroundColor: Color(0xffefefff),
-        body: () {
-          // _tabController = TabController(length: stations.length, vsync: vsync)
-
-          return SizedBox(
+        body: SizedBox(
               width: pageSize.width,
               height: pageSize.height,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  // RouteMap(onMapReady: (controller) {
-                  //   _mapController = controller;
-                  // }),
+                  RouteMap(onMapReady: (controller) {
+                    _mapController = controller;
+                  }, floatingButton: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment(Alignment.bottomRight.x, Alignment.bottomRight.y - 0.4),
+                        child: FloatingActionButton.small(
+                          onPressed: () {},
+                          child: const Icon(
+                            Icons.refresh,
+                            size: 30,
+                          )
+                        ),
+                      ), Align(
+                        alignment: Alignment(Alignment.bottomRight.x, Alignment.bottomRight.y - 0.2),
+                        child: FloatingActionButton.small(
+                          onPressed: () {},
+                          child: const Icon(
+                            Icons.location_on,
+                            size: 30,
+                          )
+                        ),
+                      ), Align(
+                        alignment: Alignment(Alignment.bottomRight.x, Alignment.bottomRight.y),
+                        child: FloatingActionButton.small(
+                          onPressed: () {},
+                          child: const Icon(
+                            Icons.location_on,
+                            size: 30,
+                          )
+                        ),
+                      ),
+                    ],
+                  )),
                   RouteCard(pageController: _pageController)
                 ],
-              ));
-        }());
+              )
+        )
+    );
   }
 }
