e575f04c654596a4f621b64a5194ef42193827a2
gunyu1019 <gunyu1019@gmail.com>
2024-09-21 10:17:44 +0000

    [Feat] Add backend parts of route card

--- lib/widgets/inner_bus/route_card.dart
+++ lib/widgets/inner_bus/route_card.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:knu_transport/models/station_info.dart';
+import 'package:knu_transport/models/timetable.dart';
 import 'package:knu_transport/providers/initalize.dart';
 import 'package:knu_transport/widgets/text_skeleton.dart';
-
 
 class RouteCard extends ConsumerStatefulWidget {
   final PageController pageController;
@@ -52,19 +52,30 @@ class _RouteCardState extends ConsumerState<RouteCard> {
     const style = TextStyle(
       fontSize: 18,
       fontWeight: FontWeight.w500,
-      color: Color(0xff1a1a1a),
+      color: Color(0xff585858),
     );
     return Container(
-      alignment: Alignment.centerRight,
-      child: skeleton ? 
-        SkeletonText.fromText(text: text, style: style) : 
-        Text(
-          // "${currentTimetable[0].hour}:${currentTimetable[0].minute} 후 (${currentTimetable[0].sessionIndex}회차 버스) 출발 예정",
-          text,
-          textAlign: TextAlign.end,
-          style: style,
-        )
-      );
+        alignment: Alignment.centerRight,
+        child: skeleton
+            ? SkeletonText.fromText(text: text, style: style)
+            : Text(
+                text,
+                textAlign: TextAlign.end,
+                style: style,
+              ));
+  }
+
+  String timetableFormat(Timetable timetable, [TimeOfDay? now]) {
+    now = now ?? TimeOfDay.now();
+    int nowTotalMinute = now.hour * 60 + now.minute;
+    int timetableTotalMinute = timetable.totalMinute();
+    if (timetableTotalMinute - nowTotalMinute <= 1) {
+      return "곧";
+    } else if (timetableTotalMinute - nowTotalMinute <= 60) {
+      return "${timetableTotalMinute - nowTotalMinute}분 후";
+    } else {
+      return "${(timetableTotalMinute - nowTotalMinute) ~/ 60} 시간 ${(timetableTotalMinute - nowTotalMinute) % 60}분 후";
+    }
   }
 
   Widget item(BuildContext context, int index) {
@@ -73,20 +84,19 @@ class _RouteCardState extends ConsumerState<RouteCard> {
 
     // title
     final name = station.value?[index].name ?? "새롬관(회차)";
-    final direction = index + 2 < (station.value?.length ?? 0) ?
-      "${station.value?[index + 1].name}, ${station.value?[index + 2].name}" : (
-        index + 1 < (station.value?.length ?? 0) ? 
-        station.value![index + 1].name : 
-        null
-      );
+    final direction = index + 2 < (station.value?.length ?? 0)
+        ? "${station.value?[index + 1].name}, ${station.value?[index + 2].name}"
+        : (index + 1 < (station.value?.length ?? 0)
+            ? station.value![index + 1].name
+            : null);
 
     // description
     TimeOfDay now = TimeOfDay.now();
-    var currentTimetable = timetable.hasValue ?
-     timetable.value![index].where((element) => element.compareTo(now) >= 0).toList() : 
-     List.empty();
-    
-
+    var currentTimetable = timetable.hasValue
+        ? timetable.value![index]
+            .where((element) => element.compareTo(now) >= 0)
+            .toList()
+        : List.empty();
 
     return Padding(
       padding: const EdgeInsets.all(20),
@@ -98,12 +108,20 @@ class _RouteCardState extends ConsumerState<RouteCard> {
             children: [
               titleText(name),
               const SizedBox(width: 3),
-              direction != null ? subtitleText(direction) : const SizedBox.shrink()
+              direction != null
+                  ? subtitleText(direction)
+                  : const SizedBox.shrink()
             ],
           ),
           const Spacer(flex: 1),
-          currentTimetable.isNotEmpty ? description("${currentTimetable[0].format(context)} 후 (${currentTimetable[0].sessionIndex}회차 버스) 출발 예정") : description("운행 종료"),
-          currentTimetable.length > 1 ? description("${currentTimetable[1].format(context)} 후 (${currentTimetable[1].sessionIndex}회차 버스) 출발 예정") : const SizedBox.shrink(),
+          currentTimetable.isNotEmpty
+              ? description(
+                  "${timetableFormat(currentTimetable[0])} (${currentTimetable[0].sessionIndex}회차 버스) 출발 예정")
+              : description("운행 종료"),
+          currentTimetable.length > 1
+              ? description(
+                  "${timetableFormat(currentTimetable[1])} (${currentTimetable[1].sessionIndex}회차 버스) 출발 예정")
+              : const SizedBox.shrink(),
         ],
       ),
     );
@@ -115,21 +133,17 @@ class _RouteCardState extends ConsumerState<RouteCard> {
     final pageViewerSize = Size(mediaQuery.size.width, 200);
     final timetable = ref.watch(dataTimetableProvider);
     final station = ref.watch(dataStationInfoProvider);
-    
+
     skeleton = !(station.hasValue && timetable.hasValue);
-  
-    return 
-      SizedBox(
+
+    return SizedBox(
         width: pageViewerSize.width,
         height: pageViewerSize.height,
         child: skeleton
-           ? item(context, -1)
-           : PageView.builder(
-              controller: widget.pageController,
-              itemCount: station.value!.length,
-              itemBuilder: item
-            )
-      );
+            ? item(context, -1)
+            : PageView.builder(
+                controller: widget.pageController,
+                itemCount: station.value!.length,
+                itemBuilder: item));
   }
 }
- 
